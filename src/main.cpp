a98016ca9c44124519a1ffcde851667fb4a96855
Bhavith C <bhavithc.acharya@gmail.com>
2022-05-11 13:45:59 +0530

    refactor: Add command line support for the message
    - User can provide his message via command line tool
    - Number of words are restricted to 10 words
    
    Issue-ID: dummy-id
    Signed-off-by: Bhavith C <bhavithc.acharya@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
+#include <sstream>
 #include "IntentRecognizer.h"
+
+#define MAX_WORDS_SUPPORTED 10
+
+void printHelp()
+{
+    std::stringstream ss;
+    ss << "Usage:\n";
+    ss << "\tIntentRecognizer <your message here>\n";
+    ss << "\t@remark: message should not exceed " << MAX_WORDS_SUPPORTED << " word(s)";
+    std::cout << ss.str() << "\n";
+}
 
 int main(int argc, char** pArgv)
 {
+    std::string sentence;
     std::string intent;
+
+    if (argc == 1) {
+        std::cerr << "No message provided ! \n";
+        printHelp();
+        return -1;
+    }
+
+    if (argc > MAX_WORDS_SUPPORTED) {
+        std::cerr << "Message has more than " << MAX_WORDS_SUPPORTED << " words \n";
+        printHelp();
+        return -1;
+    }
+
+    for (auto i = 1; i < argc; i++) {
+        sentence += pArgv[i];
+        if (i != argc - 1) {
+            sentence += " ";
+        }
+    }
+
     IntentRecognizer ir;
-    ir.recognizeIntent("Need to know weather today.", intent);
-    std::cout << "Intent is : " << intent << "\n";
+    ir.recognizeIntent(sentence, intent);
+    std::cout << sentence << " => (Intent: " << intent << ")\n";
     return 0;
 }
